a86f24c986c44824be89bf46c8c7a2779be263a6
ISHII Masaru <noitorai555@gmail.com>
2015-05-15 12:34:05 +0900

    remove debug code

--- bin/release-tsv.sh
+++ bin/release-tsv.sh
@@ -4,13 +4,10 @@ detect_unnecessary_suffix() {
         echo $1 |awk -F'.' '{printf ".%s.%s\n", $(NF-1), $NF}'
 }
 
-#suffix=$1
-#shift
 list="$@"
 
 for i in $list
 do
         suffix=`detect_unnecessary_suffix $i`
-        echo $suffix
         echo mv $i `basename $i $suffix`.tsv
 done
