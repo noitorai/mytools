c14e9d72287818662270be86f10c3fdc938a384b
ISHII Masaru <noitorai555@gmail.com>
2015-05-15 12:13:55 +0900

    remove suffix argument

--- bin/release-tsv.sh
+++ bin/release-tsv.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-suffix=$1
-shift
+detect_unnecessary_suffix() {
+        echo $1 |awk -F'.' '{printf ".%s.%s\n", $(NF-1), $NF}'
+}
+
+#suffix=$1
+#shift
 list="$@"
 
-for i in $list; do echo mv $i `basename $i $suffix`.tsv ; done
+for i in $list
+do
+        suffix=`detect_unnecessary_suffix $i`
+        echo $suffix
+        echo mv $i `basename $i $suffix`.tsv
+done
